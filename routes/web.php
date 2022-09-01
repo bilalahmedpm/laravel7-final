56a7bdc15335e7d54eb446740a9f99b1c0071064
Bilal Ahmad <bilalmujahid89@gmail.com>
2022-09-02 02:39:33 +0500

    Email Verification Added

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,16 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/client/create', 'ClientController@create')->name('client.create');
-Route::post('/client/store', 'ClientController@store')->name('client.store');
-Route::get('/client/index', 'ClientController@index')->name('client.index');
-Route::get('/client/edit/{id}', 'ClientController@edit')->name('client.edit');
-Route::post('/client/update/{id}', 'ClientController@update')->name('client.update');
-Route::get('/client/delete/{id}', 'ClientController@destroy')->name('client.delete');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/client/create', 'ClientController@create')->name('client.create');
+    Route::post('/client/store', 'ClientController@store')->name('client.store');
+    Route::get('/client/index', 'ClientController@index')->name('client.index');
+    Route::get('/client/edit/{id}', 'ClientController@edit')->name('client.edit');
+    Route::post('/client/update/{id}', 'ClientController@update')->name('client.update');
+    Route::get('/client/delete/{id}', 'ClientController@destroy')->name('client.delete');
+    });
+
 
